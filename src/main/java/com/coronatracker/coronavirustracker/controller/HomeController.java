ff1ed32177c3907479d8d5e2d2e66a251fdb2b88
ozgeonec <ozgenuronec@gmail.com>
2020-03-25 16:33:34 +0300

    finished

--- src/main/java/com/coronatracker/coronavirustracker/controller/HomeController.java
+++ src/main/java/com/coronatracker/coronavirustracker/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.coronatracker.coronavirustracker.controller;
 
+import com.coronatracker.coronavirustracker.model.LocationStats;
+import com.coronatracker.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class HomeController {
+    
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+    List<LocationStats> allStats= coronaVirusDataService.getAllStats();
+    int totalReportCases = allStats.stream().mapToInt(stat -> stat.getLatestTotal()).sum();
+    int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+    @GetMapping("/")
+    public String Home(Model model){
+        model.addAttribute("locationStat", coronaVirusDataService.getAllStats());
+        return "home";
+    }
+
 }
--- src/main/java/com/coronatracker/coronavirustracker/model/LocationStats.java
+++ src/main/java/com/coronatracker/coronavirustracker/model/LocationStats.java
@@ -1,4 +1,50 @@
 package com.coronatracker.coronavirustracker.model;
 
 public class LocationStats {
+    private String state;
+    private String country;
+    private int latestTotal;
+    private int diffFromPrevDay;
+
+
+    public int getLatestTotal() {
+        return latestTotal;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLatestTotal(int latestTotal) {
+        this.latestTotal = latestTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "stats='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotal +
+                '}';
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
 }
